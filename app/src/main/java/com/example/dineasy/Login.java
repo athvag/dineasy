1556a2937c99b0b739d7916f5d2aef64ab28878f
skkom <sk.komianos@gmail.com>
2020-04-27 22:53:38 +0300

    v0.01

---
+++ app/src/main/java/com/example/dineasy/Login.java
@@ -0,0 +1,14 @@
+package com.example.dineasy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login2);
+    }
+}
