09ec4fe4e3e70ff7048a6c38091be50cd4ff7ba7
skkom <sk.komianos@gmail.com>
2020-04-27 22:30:55 +0300

    v0.01

---
+++ app/src/main/java/com/example/dineasy/ManageBusiness.java
@@ -0,0 +1,14 @@
+package com.example.dineasy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManageBusiness extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.manage_business);
+    }
+}
