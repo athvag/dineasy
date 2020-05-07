490d3a2d6db9bf56fd6e171573a5b63d9f2e1894
chris <chrisntou@gmail.com>
2020-05-07 12:36:19 +0300

    "Eggrafi" button redirects to login2.xml "Sundesi" button redirects to basic_user.xml

---
+++ app/src/main/java/com/example/dineasy/basic_user.java
@@ -0,0 +1,15 @@
+package com.example.dineasy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class basic_user extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.basic_user);
+    }
+
+}
