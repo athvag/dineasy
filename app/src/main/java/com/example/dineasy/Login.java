097a90d6f31b202680d7913e19cf52c469e1ce3c
athvag <athanasiouvag@yahoo.com>
2020-05-09 21:44:25 +0300

    Login+Logout+QR Scan+Session

--- app/src/main/java/com/example/dineasy/Login.java
+++ app/src/main/java/com/example/dineasy/Login.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.se.omapi.Session;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,18 +25,50 @@ public class Login extends AppCompatActivity {
         Info = (TextView) findViewById(R.id.textError);
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+           public void onClick(View v) {
                 validate(Name.getText().toString(),Password.getText().toString());
-            }
-        });
+           }
+      });
     }
-    private void validate(String username, String password){
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+       checkSession();
+
+    }
+
+    private void checkSession() {
+        //check if user is logged in
+        //if user is logged in move to basic user
+        SessionManagement sessionManagement = new SessionManagement(Login.this);
+        int userID = sessionManagement.getSession();
+        if(userID != -1){
+            //user is logged in and move to next
+            moveToNextActivity();
+        }
+        else{
+            //do nothing
+        }
+    }
+   private void validate(String username, String password){
         if((username.equals ("admin")) && (password.equals  ("1234"))){
-            Intent intent = new Intent (Login.this,basic_user.class);
-            startActivity(intent);
-        } else {
-        Info.setText("Λάθος Όνομα Χρήστη/Κωδικός.");
+            user user = new user(1,"temp");
+            SessionManagement sessionManagement = new SessionManagement(Login.this);
+            sessionManagement.saveSession(user);
+            //2
+            moveToNextActivity();
+      } else {
+       Info.setText("Λαθος Όνομα Χρήστη/Κωδικός.");
         }
     }
 
+
+
+    private void moveToNextActivity() {
+        Intent intent = new Intent(Login.this,basic_user.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/dineasy/basic_user.java
+++ app/src/main/java/com/example/dineasy/basic_user.java
@@ -1,8 +1,17 @@
 package com.example.dineasy;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class basic_user extends AppCompatActivity {
 
@@ -10,6 +19,52 @@ public class basic_user extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.basic_user);
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA}, PackageManager.PERMISSION_GRANTED);
+    }
+    public void scanButton(View view){
+        IntentIntegrator intentIntegrator = new IntentIntegrator(this);
+        intentIntegrator.setPrompt("Scan the table's barcode");
+        intentIntegrator.setOrientationLocked(false);
+        intentIntegrator.setBeepEnabled(false);
+        intentIntegrator.initiateScan();
+
+
+
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        String scanContent = scanningResult.getContents();
+        if(scanContent != null){
+        if(scanContent.equals ("1234")){
+
+               Intent intent = new Intent(basic_user.this, basic_user_after_qr.class);
+               startActivity(intent);
+           }
+        else{
+            Intent intent = new Intent(basic_user.this, basic_user.class);
+            startActivity(intent);
+        }}
+        else{
+            Intent intent = new Intent(basic_user.this, basic_user.class);
+            startActivity(intent);
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    public void logout(View view){
+        //this method will remove session and open login screen
+        SessionManagement sessionManagement = new SessionManagement(basic_user.this);
+        sessionManagement.removeSession();
+
+        moveToMainScreen();
+    }
+
+    private void moveToMainScreen() {
+        Intent intent = new Intent(basic_user.this,Login.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
 }
